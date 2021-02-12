09ff5ec4eb3901475460789bbc875526fd0a9f4c
shensju <shensju@163.com>
2021-02-12 23:17:30 +0800

    update LinkedList.java

--- java/02_LinkedList/LinkedList.java
+++ java/02_LinkedList/LinkedList.java
@@ -32,7 +32,7 @@ public class LinkedList<E> {
         }
     }
 
-    private Node dummyHead;
+    private Node dummyHead; // 虚拟头结点
     private int size;
 
     public LinkedList() {
@@ -84,5 +84,114 @@ public class LinkedList<E> {
      */
     public void addLast(E e) {
         add(size, e);
+    }
+
+    /**
+     * 获得链表的第index（0-based）个位置的元素
+     * 在链表中不是一个常用的操作，练习用: )
+     * @param index
+     * @return
+     */
+    public E get(int index) {
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Get failed. Illegal index.");
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++)
+            cur = cur.next;
+        return cur.e;
+    }
+
+    /**
+     * 获得链表的第一个元素
+     * @return
+     */
+    public E getFirst() {
+        return get(0);
+    }
+
+    /**
+     * 获得链表的最后一个元素
+     * @return
+     */
+    public E getLast() {
+        return get(size - 1);
+    }
+
+    /**
+     * 修改链表的第index（0-based）个位置的元素为e
+     * 在链表中不是一个常用的操作，练习用: )
+     * @param index
+     * @param e
+     */
+    public void set(int index, E e) {
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Set failed. Illegal index.");
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++)
+            cur = cur.next;
+        cur.e = e;
+    }
+
+    /**
+     * 查找链表中是否有元素e
+     * @param e
+     * @return
+     */
+    public boolean contains(E e) {
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            if (cur.e == e)
+                return true;
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    /**
+     * 从链表中删除index（0-based）位置的元素，返回被删除的元素
+     * 在链表中不是一个常用的操作，练习用: )
+     * @param index
+     * @return
+     */
+    public E remove(int index) {
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Remove failed. Illegal index.");
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
+        }
+        Node retNode = prev.next;
+        prev.next = retNode.next;
+        retNode.next = null;
+        size--;
+        return retNode.e;
+    }
+
+    /**
+     * 从链表中删除第一个元素，返回被删除的元素
+     * @return
+     */
+    public E removeFirst() {
+        return remove(0);
+    }
+
+    /**
+     * 从链表中删除最后一个元素，返回被删除的元素
+     * @return
+     */
+    public E removeLast() {
+        return remove(size - 1);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        Node cur = dummyHead.next;
+        while (cur != null) {
+            res.append(cur + "->");
+            cur = cur.next;
+        }
+        res.append("NULL");
+        return res.toString();
     }
 }
