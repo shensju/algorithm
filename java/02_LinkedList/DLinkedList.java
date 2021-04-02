ce5e376fd87bf0402fff6dbb5b16088894c5b9af
shensju <shensju@163.com>
2021-04-02 23:40:10 +0800

    DLinkedList add methods updated

--- java/02_LinkedList/DLinkedList.java
+++ java/02_LinkedList/DLinkedList.java
@@ -19,6 +19,11 @@ public class DLinkedList<E> {
             this.next = next;
             this.prev = prev;
         }
+
+        @Override
+        public String toString() {
+            return data.toString();
+        }
     }
 
     // 双向链表的首结点
@@ -30,10 +35,43 @@ public class DLinkedList<E> {
     // 双向链表的结点数
     private int size;
 
+    /**
+     * 初始化双向链表
+     */
     public DLinkedList() {
         first = null;
         last = first;
         size = 0;
+    }
+
+    /**
+     * 链表中的结点个数
+     * @return
+     */
+    public int size() {
+        return size;
+    }
+
+    /**
+     * 判断链表是否为空，若为空返回true，否则返回false
+     * @return
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * 将新结点插入到链表的第index[0, size]个位置
+     * @param index
+     * @param e
+     */
+    public void add(int index, E e) {
+        if (index < 0 || index > size)
+            throw new IllegalArgumentException("Add failed. Illegal index.");
+        if (index == size)
+            linkLast(e);
+        else
+            linkBefore(e, node(index));
     }
 
     /**
@@ -96,5 +134,66 @@ public class DLinkedList<E> {
         size++;
     }
 
-}
+    /**
+     * 将新结点插入到指定的结点前面
+     * @param e
+     * @param succ
+     */
+    public void linkBefore(E e, Node<E> succ) {
+        Node<E> p = succ.prev;
+        Node<E> newNode = new Node<>(p, e, succ);
+        succ.prev = newNode;
+        if (p == null)
+            first = newNode;
+        else
+            p.next = newNode;
+        size++;
+    }
 
+    /**
+     * 返回链表的第index[0, size - 1]个结点
+     * @param index
+     * @return
+     */
+    private Node<E> node(int index) {
+        if (index < 0 || index >= size)
+            throw new IllegalArgumentException("Get node failed. Illegal index.");
+        if (index < (size >> 1)) {
+            Node<E> target = first;
+            for (int i = 0; i < index; i++)
+                target = target.next;
+            return target;
+        } else {
+            Node<E> target = last;
+            for (int i = size - 1; i > index; i--)
+                target = target.prev;
+            return target;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        Node<E> cur = first;
+        while (cur != null) {
+            res.append(cur + "->");
+            cur = cur.next;
+        }
+        res.append("NULL");
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        DLinkedList<Integer> list = new DLinkedList<>();
+        for (int i = 0; i < 5; i++) {
+            list.add(i);
+            System.out.println(list);
+        }
+        list.add(0, 10);
+        System.out.println(list);
+        list.add(6, 11);
+        System.out.println(list);
+        list.add(3, 20);
+        System.out.println(list);
+    }
+}
