0d028ed70f8daf28f08146c621f3204cfd5e80cc
shensju <shensju@163.com>
2021-04-28 23:58:34 +0800

    KMP.c | nextval(), main()

--- c/04_String/KMP.c
+++ c/04_String/KMP.c
@@ -25,7 +25,7 @@ Status StrAssign(String T, char *chars) {
 
 /* 清空串 */
 Status ClearString(String S) {
-    T[0] = 0；
+    S[0] = 0;
     return OK; 
 }
 
@@ -76,7 +76,7 @@ void get_next(String T, int *next) {
     j = 0;
     next[1] = 0;
     while (i < T[0]) {
-        if (j == 0 || T[i] == T[Jj) {  /* T[i]表示后缀的单个字符，T[j]表示前缀的单个字符 */
+        if (j == 0 || T[i] == T[j]) {  /* T[i]表示后缀的单个字符，T[j]表示前缀的单个字符 */
             ++i;
             ++j;
             next[i] = j;
@@ -105,4 +105,84 @@ int Index_KMP(String S, String T, int pos) {
         return i - T[0];
     else
         return 0;
+}
+
+/* 求模式串T的next函数修正值并存入数组nextval */
+void get_nextval(String T, int *nextval) {
+    int i, j;
+    i = 1;
+    j = 0;
+    nextval[1] = 0;
+    while (i < T[0]) {
+        if (j == 0 || T[i] == T[j]) {  /* T[i]表示后缀的单个字符，T[j]表示前缀的单个字符 */
+            ++i;
+            ++j;
+            if (T[i] != T[j])          /* 若当前字符与前缀字符不同，则当前的j为nextval在i位置的值 */
+                nextval[i] = j;
+            else                       /* 如果与前缀字符相同，则将前缀字符的nextval值赋值给nextval在i位置的值 */
+                nextval[i] = nextval[j];
+        } else {
+            j = nextval[j];            /* 若字符不相同，则j值回溯 */
+        }
+    }
+}
+
+
+int Index_KMP2(String S, String T, int pos) {
+    int i = pos;               /* i用于主串S中当前位置下标值，若pos不为1，则从pos位置开始匹配 */
+    int j = 1;                 /* j用于子串T中当前位置下标值 */
+    int next[255];             /* 定义一next数组 */
+    get_nextval(T, next);      /* 对串T作分析，得到next数组 */
+    while (i <= S[0] && j <= T[0]) {
+        if (j == 0 || S[i] == T[j]) {  /* 两字母相等则继续，与朴素算法增加了j=0判断 */
+            ++i;
+            ++j;
+        } else {
+            j = next[j];  /* 指针后退重新开始匹配。j退回合适的位置，i值不变 */
+        }
+    }
+    if (j > T[0])
+        return i - T[0];
+    else
+        return 0;
+}
+
+int main() {
+    int i, *p;
+    String s1, s2;
+
+    StrAssign(s1, "abcdex");
+    printf("字串为：");
+    StrPrint(s1);
+    i = StrLength(s1);
+    p = (int*) malloc ((i + 1) * sizeof(int));
+    get_next(s1, p);
+    printf("Next为：");
+    NextPrint(p, StrLength(s1));
+    printf("\n");
+ 
+    StrAssign(s1,"00000000000000000000000000000000000000000000000001");
+    printf("主串为: ");
+    StrPrint(s1);
+    StrAssign(s2,"0000000001");
+    printf("子串为: ");
+    StrPrint(s2);
+    printf("\n");
+    printf("主串和子串在第 %d 个字符处首次匹配（朴素模式匹配算法）\n", Index(s1, s2, 1));
+    printf("主串和子串在第 %d 个字符处首次匹配（KMP算法） \n", Index_KMP(s1, s2, 1));
+    printf("主串和子串在第 %d 个字符处首次匹配（KMP改良算法） \n", Index_KMP2(s1, s2, 1));
+
+    return 0;
+
+    /*
+        字串为：abcdex
+        Next为：011111
+
+        主串为: 00000000000000000000000000000000000000000000000001
+        子串为: 0000000001
+
+        主串和子串在第 41 个字符处首次匹配（朴素模式匹配算法）
+        主串和子串在第 41 个字符处首次匹配（KMP算法）
+        主串和子串在第 41 个字符处首次匹配（KMP改良算法）
+     */
 }
