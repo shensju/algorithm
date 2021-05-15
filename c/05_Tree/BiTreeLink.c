bd5e93d65415a4cac3f9620743cacc6c4bf87d77
shensju <shensju@163.com>
2021-05-15 23:57:40 +0800

    BiTreeLink.c | update DestroyBiTree method

--- c/05_Tree/BiTreeLink.c
+++ c/05_Tree/BiTreeLink.c
@@ -66,3 +66,16 @@ void CreateBiTree(BiTree *T) {
         CreateBiTree(&(*T) -> rchild);  /* 构造右子树 */
     }
 }
+
+/* 初始条件: 二叉树T存在。操作结果: 销毁二叉树T */
+void DestroyBiTree(BiTree *T) {
+    if(*T) {
+        if((*T) -> lchild)  /* 有左孩子 */
+            DestroyBiTree(&(*T) -> lchild);  /* 销毁左孩子子树 */
+        if((*T) -> rchild)  /* 有右孩子 */
+            DestroyBiTree(&(*T) -> rchild);  /* 销毁右孩子子树 */
+        free(*T);  /* 释放根结点 */
+        *T = NULL;  /* 空指针赋0 */
+    }
+}
+
