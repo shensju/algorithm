86fdadd1e63e52130d0cd331d186cf2c1e1a9fba
shensju <shensju@163.com>
2021-05-19 23:59:12 +0800

    BiTreeLink.c | update BiTreeEmpty, BiTreeDepth method

--- c/05_Tree/BiTreeLink.c
+++ c/05_Tree/BiTreeLink.c
@@ -79,3 +79,27 @@ void DestroyBiTree(BiTree *T) {
     }
 }
 
+/* 初始条件: 二叉树T存在 */
+/* 操作结果: 若T为空二叉树,则返回TRUE,否则FALSE */
+Status BiTreeEmpty(BiTree T) {
+    if (T)
+        return FALSE;
+    else
+        return TRUE;
+}
+
+/* 初始条件: 二叉树T存在。操作结果: 返回T的深度 */
+int BiTreeDepth(BiTree T) {
+    int i, j;
+    if (!T)
+        return 0;
+    if (T -> lchild)
+        i = BiTreeDepth(T -> lchild);
+    else
+        i = 0;
+    if (T -> rchild)
+        j = BiTreeDepth(T -> rchild);
+    else
+        j = 0;
+    return i > j ? i + 1 : j + 1;
+}
